61e6c6d8df36bb0e858ae24208a04ded9b451aec
anjalijadhava010-beep <anjalijadhava010@gmail.com>
2026-07-07 19:09:05 +0530

    fix App case for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 
 import './index.css'
-iimport App from './App1.jsx'
+import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
