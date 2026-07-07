9c0e17ded3a7f4852c4365a20dbfa0595bc8fcea
anjalijadhava010-beep <anjalijadhava010@gmail.com>
2026-07-07 19:07:26 +0530

    rename App file

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 
 import './index.css'
-import App from './App.jsx'
-
-
+iimport App from './App1.jsx'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
